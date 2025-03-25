ee7cc2afa55bc7d1a700fe5d0f076a71eaadd09a
Sahil252004 <sahilamrutkar1808@gmail.com>
2025-03-25 12:04:01 +0530

    fontend url updated

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder = async (req,res) => {
 
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://meal-mingle-deployment-frontend.onrender.com";
 
     try {
         const newOrder = new orderModel({
